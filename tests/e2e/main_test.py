39088162d4dba2c369f03a2e600cd9b6f22db798
mwojtyczka <marcin.wojtyczka@databricks.com>
2024-02-26 12:03:58 +0100

    update

--- tests/e2e/main_test.py
+++ tests/e2e/main_test.py
@@ -8,7 +8,8 @@ from databricks.sdk.core import Config
 def test_main():
     w = WorkspaceClient()  # use DEFAULT profile
     config = Config()
-    job_id = get_job_id(w)
+    job_name_suffix = "marcin_project_job_qa"
+    job_id = get_job_id(w, job_name_suffix)
 
     w.jobs.run_now_and_wait(job_id)
 
@@ -18,10 +19,10 @@ def test_main():
     assert len(results.data) == 5
 
 
-def get_job_id(w):
+def get_job_id(w: WorkspaceClient, job_name_suffix: str) -> int | None:
     job_id = None
     for job in w.jobs.list():
-        if job.as_dict()['settings']['name'].endswith("marcin_project_job_qa"):
+        if job.as_dict()['settings']['name'].endswith(job_name_suffix):
             job_id = job.job_id
             break
     return job_id
