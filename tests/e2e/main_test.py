c65cc5f86ac90e06bb666905bc4459923e2b4469
mwojtyczka <marcin.wojtyczka@databricks.com>
2024-02-26 06:57:20 +0100

    refactor

--- tests/e2e/main_test.py
+++ tests/e2e/main_test.py
@@ -1,22 +1,19 @@
 # this test run the already deployed job
 
-import os
-
 from databricks.sdk import WorkspaceClient
 from databricks.sdk.service.compute import Language
-
-
-TEST_CLUSTER_ID = os.getenv('DATABRICKS_TEST_CLUSTER_ID')
-
+from databricks.sdk.core import Config
 
 # needs to run before: databricks bundle deploy --target dev
 def test_main():
     w = WorkspaceClient()  # use DEFAULT profile
-    jobs = w.jobs.list(name="marcin_project_job")
+    config = Config()
+
+    jobs = w.jobs.list(name="marcin_project_job_dev")
     job_id = next(job for job in jobs).job_id
     w.jobs.run_now_and_wait(job_id)
 
-    ctx = w.command_execution.create(cluster_id=TEST_CLUSTER_ID, language=Language.SQL).result()
+    ctx = w.command_execution.create(cluster_id=config.cluster_id, language=Language.SQL).result()
     command = "SELECT * FROM samples.nyctaxi.trips LIMIT 5;"
-    results = w.command_execution.execute(cluster_id=TEST_CLUSTER_ID, command=command, context_id=ctx.id, language=Language.SQL).result()
+    results = w.command_execution.execute(cluster_id=config.cluster_id, command=command, context_id=ctx.id, language=Language.SQL).result()
     assert len(results.results.data) == 5
